7796a2acc3b8775b030803b417d8a3ac06723551
caterinatorelli <caterina.torelli2@studio.unibo.it>
2024-12-30 18:14:22 +0000

    test 1

--- src/progetto.c
+++ src/progetto.c
@@ -1,111 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-
-typedef struct {
-    int x, y;
-    long costo;
-} Cell;
-
-int cellscomparator (void *a,void *b) { 
-    Cell *cell1 = (Cell *) a;
-    Cell *cell2 = (Cell *) b;
-     if ((*cell1).costo < (*cell2).costo) {
-        return -1; // Il costo di cell1 è minore, cell1 viene prima
-    } else if ((*cell1).costo > (*cell2).costo) {
-        return 1;  // Il costo di cell1 è maggiore, cell1 viene dopo
-    } else {
-        return 0;  // I costi sono uguali
-    }
-
-}
-
-int costCalculator (int Ccell, int Cheight, int height1, int height2)
+int *read_dimension(FILE *filein)
 {
-    int diff=height1-height2;
-    int cost=Ccell + Cheight *(diff * diff);
-    return cost;
+    int i = 0;
+
+    int *dim = (int *)malloc(4 * sizeof(dim));
+    for (i = 0; i < 4; i++)
+    {
+        fscanf(filein, "%d", &dim[i]);
+    }
+    return dim;
 }
 
-int main() {
-    FILE *file;
-    int Ccell, Cheight, n, m, i, j;
-    int **H, **cost, **predecessor;
-    
-    if (argc != 2) {
-        printf("Uso: %s nome_file_input\n", argv[0]);
-        return 1;
-    }
+int **load_matrix(FILE *filein, int rows, int col)
+{
+    int i = 0, j = 0;
+    int **matrix;
 
-    file = fopen(argv[1], "r");
-    if (file == NULL) {
-        printf("Errore nell'aprire il file\n");
-        return 1;
-    }
+    /*inizializzo la matrice con calloc in maniera da pulire le memeorie*/
+    matrix = (int **)calloc(rows, sizeof(*matrix));
+    assert(matrix != NULL);
+    printf("%d ", matrix[i][j]);
 
-    // Lettura dei parametri dal file
-    fscanf(file, "%d", &Ccell);
-    fscanf(file, "%d", &Cheight);
-    fscanf(file, "%d", &n);
-    fscanf(file, "%d", &m);
+    for (i = 0; i < rows; i++)
+    {
 
-    // Creazione della matrice delle altezze
-    H = (int **) malloc(n * sizeof(int *));
-    for (i = 0; i < n; i++) {
-        H[i] = (int *) malloc(m * sizeof(int));
-        for (j = 0; j < m; j++) {
-            fscanf(file, "%d", &H[i][j]);
+        matrix[i] = (int *)calloc(col, sizeof(**matrix));
+
+        assert(matrix[i] != NULL);
+        /*avanza con il riempimento della riga solo quando trova numeri consecutivi*/
+        for (j = 0; j < col;)
+        {
+            fscanf(filein, "%d", &matrix[i][j]);
+            printf("%d ", matrix[i][j]);
+            j++;
         }
     }
-    fclose(file);
+    return matrix;
+}
 
-    int direction[4][2]={{0,1}, {0,-1}, {-1,0}, {1,0}} ; // destra sinistra su giu    
-    
-    Cell *heap = (Cell *) malloc(n * m * sizeof(Cell));
-    int heapSize = 0;
+int main(int argc, char const *argv[])
+{
+    int rows, cols;
+    int Ccell;
+    int Cheight;
+    int *r_c;
+    int **matrix;
 
-    // Inizializza la cella di partenza
-    cost[0][0] = 0;
-    heap[heapSize++] = (Cell) {0, 0, 0};
-
-    while (heapSize > 0) {
-        qsort(heap, heapSize, sizeof(Cell), cellscomparator);
-        Cell current = heap[heapSize--];
-        int x =current.x ;
-        int y= current.y;
-
-        for (int d = 0; d < 4; d++) {
-            int nx = x + direction[d][0];
-            int ny = y + direction[d][1];
-
-            if (nx < n && nx >= 0 && ny >= 0 && ny < m)
-            {
-               int costoCella=cost [x][y];
-               int costoMovimento= costCalculator(Ccell,Cheight,H[x][y], H[nx][ny]);
-               int costotot=costoCella + costoMovimento ;
-               if (costotot < cost[nx][ny]) {
-        // Aggiorno il costo della cella (nx, ny) con il nuovo costo calcolato
-        cost[nx][ny] = costotot;
-
-        predecessor[nx][ny] = x * m + y;
-               }
-        
-            }
-        }
+    FILE *filein = stdin;
+    if (argc != 2)
+    {
+        fprintf(stderr, "Invocare il programma con: %s matrice\n", argv[0]);
+        return EXIT_FAILURE;
     }
-          printf("Costo minimo: %d\n", cost[n-1][m-1]);
-
-    // Pulizia della memoria
-    for (i = 0; i < n; i++) {
-        free(H[i]);
-        free(cost[i]);
-        free(predecessor[i]);
+    filein = fopen(argv[1], "r");
+    if (filein == NULL)
+    {
+        fprintf(stderr, "Can not open %s\n", argv[1]);
+        return EXIT_FAILURE;
     }
-    free(H);
-    free(cost);
-    free(predecessor);
-    free(heap);
-
-    return 0;
-
-
+    r_c = read_dimension(filein);
+    Ccell = r_c[0];
+    Cheight = r_c[1];
+    rows = r_c[2];
+    cols = r_c[3];
+    matrix = load_matrix(filein, rows, cols);
 }
---
+++ todo.md
@@ -0,0 +1,3 @@
+- leggere file e caricare in una matrice e stamparla
+- creare una matrice di struct contenente i costi delle celle adiacenti
+belmandform
